ab3c9cc8cd67696f39bb50282ad2049e72b3f1cc
Mozetsu <code.moz.00@gmail.com>
2020-05-22 15:16:14 +0100

    Add tapbar dropwdowns

--- js/parse.js
+++ js/parse.js
@@ -1,3 +1,5 @@
-export const parseEmojis = () => {
+const arr = ['lettuce', 'cucumber', 'tomato', 'pumpkin', 'cabbage'];
 
-};
+const isIncluded = (arr, item) => arr.find((vegetable) => vegetable === item);
+
+console.log(isIncluded(arr, 'lettuce'));
