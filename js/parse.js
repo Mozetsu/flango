47965819c4b8df2872cae8f5e05b98dc0ac3d62d
Mozetsu <code.moz.00@gmail.com>
2020-06-24 22:55:42 +0100

    UI finished

--- js/parse.js
+++
@@ -1,5 +0,0 @@
-const arr = ['lettuce', 'cucumber', 'tomato', 'pumpkin', 'cabbage'];
-
-const isIncluded = (arr, item) => arr.find((vegetable) => vegetable === item);
-
-console.log(isIncluded(arr, 'lettuce'));
